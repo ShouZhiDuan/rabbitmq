74191e713cf714e21c7451e19e3dfa72b728e0aa
段守志 <dszazhy1314@163.com>
2021-09-14 15:26:01 +0800

    fix code

--- src/main/java/com/rabbitmq/dsz/main_test/direct/ProducerMQ.java
+++ src/main/java/com/rabbitmq/dsz/main_test/direct/ProducerMQ.java
@@ -26,7 +26,8 @@ public class ProducerMQ {
      * 但是需要制定queue名称
      */
     public static void directProducer(Connection connection) throws IOException {
-        String directQueueName = "direct-queue-name2";
+        String exchangeName = "my-direct-exchange";//direct模式支持默认空""的交换机
+        String directQueueName = "direct-queue-name6";
         Channel channel = connection.createChannel();
         //参数一：队列名称
         //参数二：是否持久化
@@ -34,12 +35,12 @@ public class ProducerMQ {
         //参数四：消费者断开连接时是否删除队列
         //参数五：消息其他参数
         channel.queueDeclare(directQueueName, false, false, false, null);
-        String message = String.format("direct-queue-name2 当前时间：%s", new Date().getTime());
+        String message = String.format(directQueueName + "当前时间：%s", new Date().getTime());
         //参数一：交换机名称。
         //参数二：队列名称。
         //参数三：消息的其他属性-路由的headers信息。
         //参数四：消息主体
-        channel.basicPublish("", directQueueName, null, message.getBytes("UTF-8"));
+        channel.basicPublish(exchangeName, directQueueName, null, message.getBytes("UTF-8"));
     }
 
 
